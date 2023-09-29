0d234395fa6796fcfedbdbd26cfcf8427b3f6e8a
Evgeniya <eva.goddess@mail.ru>
2023-09-29 10:26:55 +0200

    check my homework PS не работает вывод на экран после переопределения метода toString() . "Cannot resolve println"

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+         Auto lada = new Auto( "Lada","Granta", 1.7,"yellow" , 2015, "Russia");
+         Auto audi = new Auto( "Audi","A8", 3.0,"black" , 2020, "Germany");
+         Auto bmw = new Auto( "BMW","Z8", 3.0,"black" , 2021, "Germany");
+         Auto kia = new Auto( "Kia","Sportage", 2.4,"red" , 2018, "South Korea");
+         Auto hyundai = new Auto( "Hyundai","Avante", 1.6,"orange" , 2016, "South Korea");
     }
+
+    //Не работает вывод на экран: System.out.println(lada);
+
 }
+

342c9955e4b9461f11b63457cc00c65afe3e2650
Evgeniya <eva.goddess@mail.ru>
2023-10-08 20:05:38 +0200

    OOP HW2 for checking

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import transport.Auto;
+import transport.Bus;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,9 +11,48 @@ public class Main {
      //    Auto hyundai = new Auto( "Hyundai","Avante", 1.6,"orange" , 2016, "South Korea");
 
        // System.out.println(lada);
-        Auto lada = new Auto("Lada","Granta", 1.7,"yellow" , 2015, "Russia", "5", "limo", 123455, 4, true);
+        Auto lada = new Auto("Lada",
+                             "Granta",
+                1.7,
+                "yellow" ,
+                2015,
+                "Russia",
+                "5",
+                "limo",
+                "123455",
+                4,
+                false,
+                new Auto.Key(true, true),
+                250);
 
-        System.out.println(lada.changeTires(1));
+        Auto kia = new Auto("KIA",
+                "Sportage",
+                2.4,
+                null ,
+                2009,
+                "Korrea",
+                "5",
+                "kupe",
+                "dw7jo",
+                2,
+                true,
+                new Auto.Key(false, false),
+                300);
+
+        Bus miniVan = new Bus("Mercedec", "mini", "black", 2015, "USA", 240);
+        Bus familyBus = new Bus("WV", "classic", "blue", 2021, "Swede", 260);
+        Bus doubleDeck = new Bus("Volvo", "original", "red", 2023, "UK", 270);
+
+        System.out.println(lada);
+        lada.changeTires(4);
+        System.out.println(lada);
+        System.out.println(kia);
+
+        System.out.println(miniVan);
+        System.out.println(familyBus);
+        System.out.println(doubleDeck);
+
+
 
     }
 
--- src/transport/Auto.java
+++ src/transport/Auto.java
@@ -1,131 +1,15 @@
 package transport;
-public class Auto {
-    private final String brand;
-    private final String model;
+
+public class Auto extends Transport{
     private double engineVolume;
-    private String color;
-    private final int year;
-    private final String country;
     private String gearBox;
     private final String bodyType;
-    private int registrationNumber;
+    private String registrationNumber;
     private final int capacityOfPassengers;
-    private boolean tireType; //0 - summer, 1 - winter
+    private boolean summerTyres; //0 - summer, 1 - winter
+    private Key key;
 
-    public Auto(String brand, String model, double engineVolume, String color, int year, String country, String gearBox, String bodyType, int registrationNumber, int capacityOfPassengers, boolean tireType) {
-
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
-        this.gearBox = gearBox;
-        this.bodyType = bodyType;
-        this.registrationNumber = registrationNumber;
-        this.capacityOfPassengers = capacityOfPassengers;
-        this.tireType = tireType;
-
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public double getEngineVolume() {
-        return engineVolume;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public int getRegistrationNumber() {
-        return registrationNumber;
-    }
-
-    public String getBodyType() {
-        return bodyType;
-    }
-
-    public int getCapacityOfPassengers() {
-        return capacityOfPassengers;
-    }
-
-    public boolean getTireType() {
-        return tireType;
-    }
-
-    public void setEngineVolume(int engineVolume) {
-        if (engineVolume <= 0) {
-            this.engineVolume = 1.5;
-        }
-        this.engineVolume = engineVolume;
-    }
-
-    public void setColor(String color) {
-        if (color == null) {
-            this.color = "white";
-        }
-        this.color = color;
-    }
-
-
-    public void setGearBox(String gearBox) {
-        if (gearBox == null) {
-            this.gearBox = "not stated";
-        }
-        this.gearBox = gearBox;
-    }
-
-    public void setRegistrationNumber(int registrationNumber) {
-        if (registrationNumber == 0) {
-            this.registrationNumber = 111;
-        }
-        this.registrationNumber = 111;
-    }
-
-    public void setTireType(boolean tireType) {
-        this.tireType = tireType;
-    }
-
-    //переопределение toString:
-    @Override
-    public String toString() {
-        return "Car: " + this.brand + " model - " + this.model + ", engine - " + this.engineVolume + ",color - " + this.color + ", color - " + this.color + ", country - " + this.country;
-
-    }
-
-    public String changeTires(int month) {
-        String result;
-        if (month < 1 || month > 12) {
-            result = "Error. Month does not exist";
-        } else if ((month > 3 && month < 10 && !this.tireType)) {
-            result = "все ок";
-        } else if ((month < 4 || month > 9 && this.tireType)) {
-            result = "все ок";
-        } else {
-            result = "сменить шины на сезонные";
-        }
-        return result;
-    }
-
-    ;
-
-
-    public class Key {
+    public static class Key {
         private final boolean remoteEngineStart;
         private final boolean keylessAccess;
 
@@ -134,24 +18,137 @@ public class Auto {
             this.keylessAccess = keylessAccess;
         }
 
-        ;
-
-        public boolean getRemoteEngineStart(int year) {
-            year = getYear();
-            if (year <= 2000 || year > 2030) {
-                System.out.println("incorrect year");
-            }
+        public boolean getRemoteEngineStart() {
             return remoteEngineStart;
         }
 
-            public boolean getKeylessAccess(int year) {
-                year = getYear();
-                if (year <= 2000 || year > 2030) {
-                    System.out.println("incorrect year");
-                }
-                return keylessAccess;
-            }
+        public boolean getKeylessAccess() {
+            return keylessAccess;
         }
+
+    @Override
+    public String toString() {
+        return (remoteEngineStart ? "удаленный запуск двигателя": "без удаленного запуска двигателя") + ", " +
+                (keylessAccess?"бесключевой доступ":"бесключевой доступ отсутствует");
+    }
+
+}
+    public Auto(String brand,
+                String model,
+                double engineVolume,
+                String color,
+                int year,
+                String country,
+                String gearBox,
+                String bodyType,
+                String registrationNumber,
+                int capacityOfPassengers,
+                boolean tireType,
+                Key key,
+                int maxSpeed) {
+        super(brand, model, color, year, country, maxSpeed);
+
+        setGearBox(gearBox);
+        if (bodyType == null || bodyType.isEmpty()){
+            bodyType = "sedan";
+        }
+        this.bodyType = bodyType;
+        setRegistrationNumber(registrationNumber);
+        if (capacityOfPassengers <= 0){
+            capacityOfPassengers = 5;
+        }
+        this.capacityOfPassengers = capacityOfPassengers;
+        this.summerTyres = summerTyres;
+        setKey(key);
+
+    }
+
+
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        }
+        this.engineVolume = engineVolume;
+    }
+
+    public String getGearBox(){
+        return gearBox;
+    }
+    public void setGearBox(String gearBox) {
+        if (gearBox == null || gearBox.isEmpty()) {
+            this.gearBox = "not stated";
+        }
+        this.gearBox = gearBox;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+    public void setRegistrationNumber(String registrationNumber) {
+        if (registrationNumber == null || registrationNumber.isEmpty()) {
+            registrationNumber = "x000xx000";
+        }
+        this.registrationNumber = registrationNumber;
+    }
+
+    public int getCapacityOfPassengers() {
+        return capacityOfPassengers;
+    }
+
+    public boolean isSummerTyres() {
+        return summerTyres;
+    }
+
+    public void setSummerTyres(boolean summerTyres) {
+        this.summerTyres = summerTyres;
+    }
+
+    public Key getKey(){
+        return key;
+    }
+
+    public void setKey(Key key){
+        if (key == null){
+            key = new Key(false, false);
+        }
+        this.key = key;
+    }
+
+    public void changeTires(int month) {
+        if ((month >= 11 && month<=12)||(month>=1 && month <= 3)){
+            summerTyres = false;
+        }
+        if (month >= 4 && month <= 10){
+            summerTyres = true;
+        }
+    }
+
+    //переопределение toString:
+    @Override
+    public String toString() {
+        return "Car: " + super.getBrand() +
+                " model - " + super.getModel() +
+                ", engine - " + this.engineVolume +
+                ",color - " + super.getColor() +
+                ", country - " + super.getCountry() +
+                ", gear - " + this.gearBox +
+                ",body - " + this.bodyType +
+                ", regNum - " + this.registrationNumber +
+                ", capacity - " + this.capacityOfPassengers +
+                (summerTyres ? "summer" : "winter") + "tire" + ", "+
+                key +
+                ", speed - " + super.getMaxSpeed();
+
+
+    }
     }
 
 //new branch
